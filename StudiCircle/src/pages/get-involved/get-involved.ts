c14386ce242109d6bbe3cfe6d5488510762091ba
Julian Droste <julian@jdroste.net>
2018-03-21 21:31:24 +0100

    added regex to filter for non - student mail

--- StudiCircle/src/pages/get-involved/get-involved.ts
+++ StudiCircle/src/pages/get-involved/get-involved.ts
@@ -10,19 +10,51 @@ import { DashboardPage } from '../dashboard/dashboard';
 })
 export class GetInvolvedPage {
 
+  mail : any;
+  password : any;
+  passwdChk : any;
+
   constructor(public navCtrl: NavController) {
+    this.mail = '';
   }
+
   goToVerifyNow(params){
+    this.verifyStudentMail();
     if (!params) params = {};
     this.navCtrl.push(VerifyNowPage);
-  }goToLogIn(params){
+  }
+
+  goToLogIn(params){
     if (!params) params = {};
     this.navCtrl.push(LogInPage);
-  }goToGetInvolved(params){
+  }
+
+  goToGetInvolved(params){
     if (!params) params = {};
     this.navCtrl.push(GetInvolvedPage);
-  }goToDashboard(params){
+  }
+
+  goToDashboard(params){
     if (!params) params = {};
     this.navCtrl.push(DashboardPage);
+  }
+
+  verifyStudentMail(){
+    //TODO add cond for when profile type is business profile
+    if(!(this.mail.includes("@student") || this.mail.includes(".edu"))){
+      if(this.mail.length>0){
+        console.log("[get-involved] Not valid student mail address: " + this.mail)
+      }else{
+        console.log("[get-involved] No String collected.")
+      }
+    }else{
+      console.log("[get-involved] Hooray! Valid Student Address: " + this.mail);
+      if(this.password === this.passwdChk && this.password.length > 0){
+        console.log("[get-involved] Hooray passwords are equal!")
+        this.goToVerifyNow({});
+      }else{
+        console.log("[get-involved] Nay passwords are not equal!")
+      }
+    }
   }
 }
