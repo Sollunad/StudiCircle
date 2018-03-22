e5d25ab56b3ddad2e6f32755b0879656a2baf39d
pm1997 <patrick.mueller97@web.de>
2018-03-22 11:45:48 +0100

    add generate random string

--- Server/Student/registration.js
+++ Server/Student/registration.js
@@ -9,7 +9,20 @@ module.exports = {
             '</body>\n' +
             '</html>';
         subject = 'Studicircle: Validate Your mail address';
+
+        let randomString = this.generateRandomString(48);
+
+        //insert userdata in database
+        // account state = onValidation
+        //db.insertNewPerson(mail,pwd,accountType,randomString);
+
+        console.log(randomString);
+        //send registration Mail
         var mailer = require('./mailer');
         mailer.sendMail(mail, html, subject, 'Error at sending verification link.', 'Send verification link.',res);
+    },
+    generateRandomString: function (length) {
+        var Crypto = require('crypto');
+        return Crypto.randomBytes(length).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
     }
 };
