0d2e4ef14dd69d90c9ac41b89559fe3d92029906
Zepnik <zepnikm@emea.roche.com>
2018-03-23 13:02:45 +0100

    Renamed Files

---
+++ Server/Student/passwordResetMail.js
@@ -0,0 +1,27 @@
+const constant = require('./constants');
+var database = require('./database');
+var mailer = require('./mailer');
+
+module.exports = {
+    reset: function (user) {
+        var uuid = mailer.generateRandomString(constant.KEY_LENGTH);
+        database.setValidationKey(user, uuid);
+
+        var mail = database.getUserData(user).mail;
+
+        let html = '<html lang="de-DE">\n' +
+                        '<head>\n' +
+                            '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\' +
+                        '</head>\n' +
+                        '<body>\n' +
+                            '<p>Please click on following link to register to reset your password for StuiCircle: <a href="http://localhost:8100/forgotPassword/' + randomString + '">Validate E-Mail</a></p>' +
+                        '</body>\n' +
+                    '</html>';
+
+        let subject = 'StudiCircle: Reset your password';
+
+        mailer.sendSimpleMail(mail, subject, html);
+
+        return true;
+    }
+}
