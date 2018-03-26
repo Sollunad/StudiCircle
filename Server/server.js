1809619e05a1157ac2b0639a7590f66a070d5163
pm1997 <patrick.mueller97@web.de>
2018-03-26 12:53:13 +0200

    add url constant to register and allow cross domain

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,16 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*.sknx.de');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -10,5 +20,6 @@ var routesStudents = require('./Student/routerStudent'); //importing route
 routesStudents(app); //register the route
 
 app.listen(8080);
+
 
 console.log('RESTful API server started on: 8080');
