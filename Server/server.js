0a461fe5b609b89f97d0641a720e0f228c6b472e
pm1997 <patrick.mueller97@web.de>
2018-03-26 14:09:00 +0200

    try to fix access controll

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,26 @@
+var bodyParser = require('body-parser');
+//var cors = require('cors');
 var express = require('express');
+
 var app = express();
 
-var bodyParser = require('body-parser');
-
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*.sknx.de');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*.sknx.de');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//
+//     next();
+// }
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
-}
+});
 
-app.use(allowCrossDomain);
+//app.use(allowCrossDomain);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
