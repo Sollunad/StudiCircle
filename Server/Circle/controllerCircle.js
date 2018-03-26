feba1c1c5c62ebb7b617cabec2297a0dec9f651b
Marcel Gehre <gehre4@googlemail.com>
2018-03-26 13:24:03 +0200

    dummy circle rest

--- Server/Circle/controllerCircle.js
+++ Server/Circle/controllerCircle.js
@@ -2,4 +2,35 @@ module.exports = {
     helloworld : function (req, res) {
         res.send('Hello World!');
     },
+
+    removeUser : function (req, res) {
+        var userId = req.body.id;
+        res.send(userId);
+    },
+
+    addUser : function (req, res) {
+        var userId = req.body.id;
+        res.send(userId);
+    },
+
+    newCircle : function (req, res) {
+        var name = req.body.name;
+        var location = req.body.location;
+        res.send(name + " " + location);
+    },
+
+    removeCircle : function (req, res) {
+        var circleId = req.body.id;
+        res.send(userId);
+    },
+
+    circlesForUserId : function (req, res) {
+        var userId = req.body.id;
+        res.send(userId);
+    },
+
+    circlesForLocation : function (req, res) {
+        var location = req.body.loc;
+        res.send(location);
+    },
 };
--- Server/Circle/routerCircle.js
+++ Server/Circle/routerCircle.js
@@ -4,4 +4,31 @@ module.exports = function(app) {
     // circle routes
     app.route('/circle/helloworld')
         .get(circle.helloworld);
+
+    // id: userId
+    app.route('/circle/removeUser')
+        .post(circle.removeUser);
+
+    // id: userId
+    app.route('/circle/addUser')
+        .post(circle.addUser);
+
+    // name: circle name
+    // loc: location
+    app.route('/circle/new')
+        .post(circle.newCircle);
+
+    // id: circleId
+    app.route('/circle/remove')
+        .post(circle.removeCircle);
+
+    // id: userId
+    // return: List circles
+    app.route('/circle/forUser')
+        .get(circle.circlesForUserId);
+
+    // loc: location
+    // return: List circles
+    app.route('/circle/forLocation')
+        .get(circle.circlesForLocation);
 };
