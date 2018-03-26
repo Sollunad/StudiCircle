396fdb8839e15e31dda295b61965dff7248a67df
epnigma <s162342@student.dhbw-mannheim.de>
2018-03-26 09:14:12 +0200

    renamed router

--- Server/server.js
+++ Server/server.js
@@ -6,8 +6,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var routes = require('./Student/routerStudent'); //importing route
-routes(app); //register the route
+var routesStudents = require('./Student/routerStudent'); //importing route
+routesStudents(app); //register the route
 
 app.listen(8080);
 
