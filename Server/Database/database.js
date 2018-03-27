12b10d0ce8d0917e3b6b49714a1921f5a89e50a2
Stefan Reschke <stefan.reschke@outlook.de>
2018-03-27 15:20:22 +0200

    Some "making DB more lovable" effort.

--- Server/Database/database.js
+++ Server/Database/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./connection.js');
 
+const User = require('./user.js');
 const Circle = require('./circle.js');
 const Location = require('./location.js');
 
@@ -8,14 +9,32 @@ const Location = require('./location.js');
  * n:m - CIRCLES AND LOCATIONS
  **/
 const CircleLocation = sequelize.define('CircleLocation', {});
-Circle.belongsToMany(Location, { as: 'locations', through: { model: CircleLocation }, foreignKey: 'circle_id' });
-Location.belongsToMany(Circle, { as: 'circles', through: { model: CircleLocation }, foreignKey: 'location_id' });
+Circle.belongsToMany(Location, { through: CircleLocation });
+Location.belongsToMany(Circle, { through: CircleLocation });
+
+/**
+ * n:m - CIRCLES AND USERS
+ **/
+const UserInCircles = sequelize.define('UserInCircles', {
+	role: Sequelize.STRING
+});
+User.belongsToMany(Circle, { through: UserInCircles });
+Circle.belongsToMany(User, { through: UserInCircles });
+
+/** in der Node-Konsole aufrufen um die Tabellen zu erzeugen/upzudaten (das gehört in den Duden) */
+function init() {
+	User.sync({force:true});
+	Circle.sync({force:true});
+	Location.sync({force:true}).then(() => {
+		CircleLocation.sync({force:true});
+		UserInCircles.sync({force:true});	
+	});
+}
 
 module.exports = {
 	Circle: Circle,
 	Location: Location,
+	User: User,
+	CircleLocation: CircleLocation,
+	UserInCircles: UserInCircles
 };
-
-// zur initialisierung der Datenbank, Stefan oder Marcel fragen
-//Circle.sync({force:true});
-//Location.sync({force:true});
