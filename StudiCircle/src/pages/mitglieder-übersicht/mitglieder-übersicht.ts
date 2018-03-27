475f60d1cae3fe9f5db6068e5449b0175b427677
Martin Thissen <martin.thissen@deutschebahn.com>
2018-03-27 15:55:34 +0200

    Added functionaltity for passing data between CircleStartseite and its subpages (Module)

--- StudiCircle/src/pages/mitglieder-übersicht/mitglieder-übersicht.ts
+++ StudiCircle/src/pages/mitglieder-übersicht/mitglieder-übersicht.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {CircleProvider} from "../../providers/api/CircleProvider";
 import {HttpClient} from "@angular/common/http";
+import {NavParams} from "ionic-angular";
 
 @Component({
   templateUrl: 'mitglieder-übersicht.html'
@@ -9,12 +10,15 @@ export class MitgliederÜbersicht {
 
   public memberList: Array<string> = [];
 
-  constructor(public circleProvider: CircleProvider, public http: HttpClient) {
+  private circleId : number;
+
+  constructor(public circleProvider: CircleProvider, public http: HttpClient, public navParams: NavParams) {
+    this.circleId = navParams.get('circleId');
   }
 
   ionViewDidLoad(){
     //TODO Use specific circleId
-    this.memberList = this.circleProvider.getMemberListByCircleId(1);
+    this.memberList = this.circleProvider.getMemberListByCircleId(this.circleId);
   }
 
 
