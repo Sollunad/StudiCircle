2a635079cad06de66b131f3481adf340c03c2a9d
Stefan Reschke <stefan.reschke@outlook.de>
2018-03-27 16:09:40 +0200

    Added dummy DB-Storage for what vaguely one day could be refered to as "modules" in project scopes sense. (maybe)

--- Server/Database/database.js
+++ Server/Database/database.js
@@ -4,6 +4,7 @@ const sequelize = require('./connection.js');
 const User = require('./user.js');
 const Circle = require('./circle.js');
 const Location = require('./location.js');
+const Module = require('./module.js');
 
 /**
  * n:m - CIRCLES AND LOCATIONS
@@ -30,13 +31,23 @@ User.references = {};
 Circle.references.user = User;
 User.references.circle = Circle;
 
+/**
+ * 1:m - CIRCLES AND MODULES
+ **/
+Circle.hasMany(Module);
+Module.belongsTo(Circle);
+
 /** in der Node-Konsole aufrufen um die Tabellen zu erzeugen/upzudaten (das gehört in den Duden) */
 function init() {
-	User.sync({force:true});
-	Circle.sync({force:true});
-	Location.sync({force:true}).then(() => {
-		CircleLocation.sync({force:true});
-		UserInCircles.sync({force:true});	
+	User.sync({force:true}).then(() => {
+		Circle.sync({force:true}).then(() => {
+			Module.sync({force:true}).then(() => {
+				Location.sync({force:true}).then(() => {
+					CircleLocation.sync({force:true});
+					UserInCircles.sync({force:true});
+				});
+			});
+		});
 	});
 }
 
@@ -45,6 +56,7 @@ module.exports = {
 	Circle: Circle,
 	Location: Location,
 	User: User,
+	Module: Module,
 	CircleLocation: CircleLocation,
 	UserInCircles: UserInCircles
 };
