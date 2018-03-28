263f5ebd47ab91b316d50a630f198f8cf10569b4
Marcel Gehre <gehre4@googlemail.com>
2018-03-28 10:09:04 +0200

    added cors

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
 var bodyParser = require('body-parser');
@@ -6,12 +7,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// ionic erlauben auf die REST Endpoints zuzugreifen
-app.get('/*',function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();
-});
+app.use(cors());
 
 var routesCircle = require('./Circle/routerCircle'); //importing route
 routesCircle(app); //register the route
