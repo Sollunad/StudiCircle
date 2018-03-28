ab638fabaf92a2f7a8192f718b1a8c5e0c21dc58
deroke <s161679@student.dhbw-mannheim.de>
2018-03-28 14:26:46 +0200

    fixed merge conflicts

--- StudiCircle/src/providers/dbprovider/dbprovider.ts
+++ StudiCircle/src/providers/dbprovider/dbprovider.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { ApiProvider } from '../api/api';
 import { Injectable } from '@angular/core';
-import {Subscription} from "rxjs/Subscription";
-import {Subject} from "rxjs/Subject";
-import {ApiResponse} from "../declarations/ApiResponse";
-import {GeoResponse} from "../declarations/GeoResponse";
+import { Subscription } from "rxjs/Subscription";
+import { Subject } from "rxjs/Subject";
+import { ApiResponse } from "../declarations/ApiResponse";
+import { GeoResponse } from "../declarations/GeoResponse";
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Circle } from '../declarations/Circle';
@@ -16,20 +16,20 @@ import { Circle } from '../declarations/Circle';
   and Angular DI.
 */
 @Injectable()
-export class DbProvider{
+export class DbProvider {
   private result: any;
 
-constructor(public http: HttpClient, private api: ApiProvider) {}
+  constructor(public http: HttpClient, private api: ApiProvider) { }
 
-public getCircles(){
-  /*this.http.get('https/api.dev.sknx.de/circle/forUser?id=1').map(res => {
-     this.res = res;
-     console.log(res);
-   });*/
-   const successSubject: Subject<boolean> = new Subject<boolean>();
+  public getCircles() {
+    /*this.http.get('https/api.dev.sknx.de/circle/forUser?id=1').map(res => {
+       this.res = res;
+       console.log(res);
+     });*/
+    const successSubject: Subject<boolean> = new Subject<boolean>();
     const subs: Subscription = this.http.get(
       'http://localhost:8080/circle/forUser?id=1').subscribe(
-      (res: ApiResponse) =>{
+      (res: ApiResponse) => {
         subs.unsubscribe();
         console.log(res);
         successSubject.next(res.httpStatus === 200);
@@ -39,35 +39,37 @@ public getCircles(){
         subs.unsubscribe();
         successSubject.next(false);
       }
-    );
-}
+      );
+  }
 
-public getCirclesByLocation(lat: number, lon: number, distance: number): Observable<Circle[]> {
-  const url = `http://localhost:8080/circle/forLocation?loc[lat]=${lat}&loc[lon]=${lon}&dist=${distance}`;
-  return this.http.get<Circle[]>(url);
-}
+  public getCirclesByLocation(lat: number, lon: number, distance: number): Observable<Circle[]> {
+    console.log(lat, lon, distance);
 
-public setLocation(lat, long) {
-  //Jesse mach mal was
-}
+    const url = `http://localhost:8080/circle/forLocation?loc[lat]=${lat}&loc[lon]=${lon}&dist=${distance}`;
+    return this.http.get<Circle[]>(url);
+  }
+
+  public setLocation(lat, long) {
+    //Jesse mach mal was
+  }
 
   public getLocationByAddress(address: string) {
     const successSubject: Subject<boolean> = new Subject<boolean>();
     const subs: Subscription = this.http.get(
-        'https://nominatim.openstreetmap.org/search/$'+ address +'?format=json&limit=1').subscribe(
-      (res: GeoResponse) =>{
+      'https://nominatim.openstreetmap.org/search/$' + address + '?format=json&limit=1').subscribe(
+      (res: GeoResponse) => {
         subs.unsubscribe();
         console.log(res);
         successSubject.next(true);
         console.log(res[0].lat, res[0].lon);
         this.setLocation(res[0].lat, res[0].lon);
-        },
-       (error: any) => {
-         console.log(error);
-         subs.unsubscribe();
-         successSubject.next(false);
-       }
-     );
+      },
+      (error: any) => {
+        console.log(error);
+        subs.unsubscribe();
+        successSubject.next(false);
+      }
+      );
   }
 
 }
