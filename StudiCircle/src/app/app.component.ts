e33248972ae52c25b2137dd273421180a243b423
elenahackstein <elenahackstein@gmx.de>
2018-04-03 13:31:24 +0200

    Testcheckin

--- StudiCircle/src/app/app.component.ts
+++ StudiCircle/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {LogInPage} from "../pages/log-in/log-in";
 import {CircleStartseite} from "../pages/circle-startseite/circle-startseite";
+import {MitgliederÜbersicht} from "../pages/mitglieder-übersicht/mitglieder-übersicht";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
 
-  rootPage: any = LogInPage;
+  rootPage: any = MitgliederÜbersicht;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
