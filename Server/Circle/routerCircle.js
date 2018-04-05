ad70e2cbcb293ffa2f1c6eace2b754f6ce866db3
Stefan Reschke <stefan.reschke@outlook.de>
2018-04-05 13:22:10 +0200

    Backend: Changing roles for given userId, circleId and newRole. No authentication required. Should only be accessed from a circle-admin session. Chaos otherwise. ¯\_(ツ)_/¯

--- Server/Circle/routerCircle.js
+++ Server/Circle/routerCircle.js
@@ -28,6 +28,12 @@ module.exports = function(app) {
     // id: circleId
     app.route('/circle/remove')
         .post(circle.removeCircle);
+		
+	// id (Number): current circle
+	// user (Number): selected users id
+	// role (String): admin | member | mod for selected user
+	app.route('/circle/rolechange')
+		.post(circle.changeRole);
 
     // return: List circles
     app.route('/circle/forUser')
