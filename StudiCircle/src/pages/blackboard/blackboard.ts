dd132f04c0eb4766b491aeb02c8e60136304cdd0
deroke <s161679@student.dhbw-mannheim.de>
2018-04-10 11:30:08 +0200

    new blackboard post

--- StudiCircle/src/pages/blackboard/blackboard.ts
+++ StudiCircle/src/pages/blackboard/blackboard.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { DbProvider } from "../../providers/dbprovider/dbprovider";
-import {ApiProvider} from "../../providers/api/api";
-import { BlackboardPost } from "../../providers/declarations/BlackboardPost";
-import { BlackboardPostPage } from "../blackboard-post/blackboard-post";
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
+import {BlackboardPost} from "../../providers/declarations/BlackboardPost";
+import {BlackboardPostPage} from "../blackboard-post/blackboard-post";
+import {CircleProvider} from "../../providers/circle-provider/CircleProvider";
 
 /**
  * Generated class for the BlackboardPage page.
@@ -20,14 +19,16 @@ export class BlackboardPage {
 
   private circleId = this.navParams.get('circleId');
   private posts = [];
+  private date = new Date();
+  private userName = 'Hans Solo';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, private dbProvider: DbProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, private circleProvider: CircleProvider) {
   }
 
   ionViewDidLoad() {
     console.log(this.circleId);
 
-    this.dbProvider.getBlackboardPosts(this.circleId).subscribe(posts => {
+    this.circleProvider.getBlackboardPosts(this.circleId).subscribe(posts => {
       console.log('getBlackboardPosts', posts);
       this.posts = posts;
     });
@@ -39,25 +40,32 @@ export class BlackboardPage {
     this.alertCtrl.create({
       title: 'Enter Text',
       inputs: [{
+        name: 'title',
+        placeholder: 'title'
+      }, {
         name: 'text',
         placeholder: 'Type here ...'
       }],
       buttons: [{
         text: 'OK',
         handler: data => {
-          const text = data.text.toString().trim();
-          if(text.length < 10) return;
+          const title = data.title.toString().trim();
+          if(title.length < 5) return;
 
-          this.insertPost(text);
+          const text = data.text.toString().trim();
+          if (text.length < 10) return;
+
+          this.insertPost(title, text);
         }
       }]
     }).present();
   }
 
-  private insertPost(text: string) {
-    console.log('new post', text);
-    this.dbProvider.insertPost(this.circleId, text).subscribe(data => {
-      console.log('data', data);
+  private insertPost(title: string, text: string) {
+    console.log('new post', title, text);
+    this.circleProvider.insertPost(this.circleId, title, text).subscribe(post => {
+      console.log('post', post);
+      this.posts.push(post);
     });
   }
 
@@ -71,7 +79,7 @@ export class BlackboardPage {
 
   private deletePost(post: BlackboardPost) {
     console.log('deletePost', post);
-    this.dbProvider.deletePost(post.postID);
+    this.circleProvider.deletePost(post.postID);
   }
 
 }
