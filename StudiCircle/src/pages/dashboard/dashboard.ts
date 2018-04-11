e797b66079717aac639ca601d407e69e1f4305e3
Martin Thissen <martin.thissen@deutschebahn.com>
2018-04-11 09:02:40 +0200

    Bug fixing

--- StudiCircle/src/pages/dashboard/dashboard.ts
+++ StudiCircle/src/pages/dashboard/dashboard.ts
@@ -8,7 +8,6 @@ import {CircleErstellenPage} from '../circle-erstellen/circle-erstellen';
 import {ApiProvider} from "../../providers/api/api";
 import {Circle} from "../../providers/declarations/Circle";
 import {CircleStartseite} from "../circle-startseite/circle-startseite";
-import {ChatPage} from "../chat/chat";
 
 @Component({
   selector: 'page-dashboard',
@@ -37,7 +36,7 @@ export class DashboardPage {
   }
 
   goToStartPage(circleId: number, circleName: string) {
-    this.navCtrl.push(ChatPage, {circleId: circleId, circleName: circleName});
+    this.navCtrl.push(CircleStartseite, {circleId: circleId, circleName: circleName});
   }
 
   goToSearch(params) {
