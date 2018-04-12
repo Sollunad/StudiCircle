31ad82e425010a6e609df7832125a81840fae0e5
JohannesKoppe <johanneskoppe@icloud.com>
2018-04-12 15:39:09 +0200

    invite buttons

--- StudiCircle/src/pages/mitglieder-übersicht/mitglieder-übersicht.ts
+++ StudiCircle/src/pages/mitglieder-übersicht/mitglieder-übersicht.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import {CircleProvider} from "../../providers/circle-provider/CircleProvider";
 import {HttpClient} from "@angular/common/http";
 import {UserInfo} from "../../providers/declarations/UserInfo";
-import {NavParams} from "ionic-angular";
+import {AlertController, NavParams} from "ionic-angular";
+import {DashboardPage} from "../dashboard/dashboard";
 
 @Component({
   templateUrl: 'mitglieder-übersicht.html'
@@ -14,7 +15,7 @@ export class MitgliederÜbersicht {
   private circleId : number;
   private isAdmin : boolean;
 
-  constructor(public circleProvider: CircleProvider, public http: HttpClient, public navParams: NavParams) {
+  constructor(public circleProvider: CircleProvider, public alertCtrl: AlertController, public http: HttpClient, public navParams: NavParams) {
     this.circleId = navParams.get('circleId');
     this.isAdmin = navParams.get('isAdmin');
   }
@@ -42,5 +43,35 @@ export class MitgliederÜbersicht {
 
   itemSelected(item: string) {
     console.log("Selected Item", item);
+  }
+
+  openInviteDialog(){
+    let alert = this.alertCtrl.create({
+      title: 'Einladen',
+      message: 'Wen einladen?',
+      inputs: [
+        {
+          type: 'text',
+          name: 'mail',
+          placeholder: 'Emailadresse'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Einladung senden',
+          handler: () => {
+            console.log('Einladung gesendet!');
+          }
+        },
+        {
+          text: 'Abbrechen',
+          role: 'cancel',
+          handler: () => {
+            console.log('Einladen abgebrochen');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
