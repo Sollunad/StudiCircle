d900b284ddd094ce948f905eae3904bbfbfaf576
Martin Thissen <martin.thissen@deutschebahn.com>
2018-04-13 21:37:48 +0200

    Styling part 2

--- StudiCircle/src/pages/timeline/timeline.ts
+++ StudiCircle/src/pages/timeline/timeline.ts
@@ -2,22 +2,24 @@ import {Component} from "@angular/core";
 import {PopoverController, NavParams} from "ionic-angular";
 import {PopoverTimelinePage} from "../popover-timeline/popover-timeline";
 import {CircleProvider} from "../../providers/circle-provider/CircleProvider";
-import {Appointment} from "../../providers/declarations/Event";
+import {Appointment} from "../../providers/declarations/Appointment";
 import * as moment from 'moment';
 
 @Component({
   selector: 'page-timeline',
   templateUrl: 'timeline.html',
 })
+
 export class TimelinePage {
 
   circleId:number;
   userRole:string= 'admin';
 
-  appointments: Appointment[] = [{id: 1, title: 'Basketball', description: 'Tolles Event', location: 'Mannheim', startDate: moment().toISOString(),
-    endDate: moment().toISOString() ,countCommits: 12,countRejections: 2, countInterested: 7},
-    {id: 2, title: 'Fußball', description: 'Beste Sport wo gibbet', location: 'Borussia-Park', startDate: moment().toISOString(),
-      endDate: moment().toISOString() ,countCommits: 42,countRejections: 12, countInterested: 27}];
+  appointments: AppointmentCard[] = [
+    {appointment: {id: 1, title: 'Basketball', description: 'Tolles Event', location: 'Mannheim', startDate: '2018-04-13T18:40:37.049Z',
+    endDate: '2018-04-14T18:40:37.049Z' ,countCommits: 12,countRejections: 2, countInterested: 7},vote:'none'},
+    {appointment: {id: 2, title: 'Fußball', description: 'Beste Sport wo gibbet', location: 'Borussia-Park', startDate: '2018-04-13T18:40:37.049Z',
+      endDate: '2018-04-14T19:40:37.049Z' ,countCommits: 42,countRejections: 12, countInterested: 27},vote:'none'}];
 
   constructor(private popoverCtrl: PopoverController,circleProvider:CircleProvider, navParams:NavParams) {
     this.circleId = navParams.get('circleId');
@@ -28,7 +30,13 @@ export class TimelinePage {
      });*/
   }
 
-
+  toggleVote(appointmentCard:AppointmentCard, vote:string) {
+     if(appointmentCard.vote===vote) {
+       appointmentCard.vote='none';
+     }else{
+       appointmentCard.vote=vote;
+     }
+  }
 
   presentPopover(event) {
 
@@ -38,5 +46,9 @@ export class TimelinePage {
       ev: event
     });
   }
+}
 
+export interface AppointmentCard{
+  appointment:Appointment
+  vote:string;
 }
