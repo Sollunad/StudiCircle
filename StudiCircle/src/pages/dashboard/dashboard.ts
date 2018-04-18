98003a827d4597f6f45cf5f7d2e63ec496e35e44
JohannesKoppe <johanneskoppe@icloud.com>
2018-04-18 17:24:12 +0200

    einladungen auf dashboard (noch auskommentiert bis backend geht)

--- StudiCircle/src/pages/dashboard/dashboard.ts
+++ StudiCircle/src/pages/dashboard/dashboard.ts
@@ -18,7 +18,11 @@ export class DashboardPage {
 
   settings: SettingsPage;
   private circles : Circle[]=[];
-  public invitList: Invitation[];
+  public invitList: Invitation[] = [
+    {cId : 1, invitId : 1, cName: "Martin"},
+    {cId : 2, invitId : 2, cName: "ist"},
+    {cId : 3, invitId : 3, cName: "1 Kek"}
+  ];
   private accountName : string;
 
   constructor(public navCtrl: NavController, private geolocation: Geolocation, private dbprovider: DbProvider, private alertCtrl: AlertController, private api: ApiProvider, private circleProvider : CircleProvider) {
@@ -58,14 +62,18 @@ export class DashboardPage {
     this.navCtrl.push(CircleErstellenPage);
   }
 
+  answerInvitation(iID: number, cID: number, answer: boolean){
+    console.log("Answered on inviteID: " + iID + " for circleID: " + cID + " accepted invite: " + answer);
+    /*this.circleProvider.answerInvite(iID, cID, answer).subscribe(data => console.log(data));*/
+  }
   ionViewWillEnter() {
     this.circleProvider.getCircles().subscribe(data => {
       this.circles = data;
       // this.showCircle(data[0]);
     });
-    this.circleProvider.getAllInvitsForUser().subscribe(
+    /*this.circleProvider.getAllInvitsForUser().subscribe(
       invitList => this.invitList = invitList
-    );
+    );*/
   }
 
   reactToInvit(circleId: number, invitId: number, status: boolean){
