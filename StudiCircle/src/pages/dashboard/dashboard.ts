ef211e347413a8dd03e1f5eac9acd1f702cd1717
JohannesKoppe <johanneskoppe@icloud.com>
2018-04-18 12:46:11 +0200

    invite gui first step

--- StudiCircle/src/pages/dashboard/dashboard.ts
+++ StudiCircle/src/pages/dashboard/dashboard.ts
@@ -19,6 +19,25 @@ export class DashboardPage {
   settings: SettingsPage;
   private circles : Circle[]=[];
   private accountName : string;
+  invites = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
 
   constructor(public navCtrl: NavController, private geolocation: Geolocation, private dbprovider: DbProvider, private alertCtrl: AlertController, private api: ApiProvider, private circleProvider : CircleProvider) {
     this.getCurrentPosition();
